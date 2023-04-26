971c33ec049867a63c79cfb93f06e9cf2addd925
Brian Phillips <brian@briancphillips.com>
2023-04-26 10:27:32 -0500

    decoupled game and ship time code

--- index.js
+++ index.js
@@ -9,7 +9,26 @@ class Ship {
     this.x = -200;
     this.y = 100;
     this.vel = 2;
+  }
+  draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "blue";
+    ctx.fillRect(this.x, this.y, 100, 50);
+  }
 
+  update(dt) {
+    console.log("Delta", dt);
+    if (this.x > canvas.width) {
+      this.x = -200;
+      this.vel > 6 ? (this.vel -= 1) : (this.vel = 2);
+    }
+
+    this.x += this.vel;
+  }
+}
+
+class Game {
+  constructor() {
     this.accumulator = 0;
     this.step = 1 / 320;
 
@@ -18,7 +37,7 @@ class Ship {
       if (lastTime !== null) {
         const diff = millis - lastTime;
         this.update(diff / 1000);
-        this.draw();
+        ship.draw();
       }
       lastTime = millis;
       requestAnimationFrame(this._frameCallback);
@@ -29,34 +48,19 @@ class Ship {
     requestAnimationFrame(this._frameCallback);
   }
 
-  draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "blue";
-    ctx.fillRect(this.x, this.y, 100, 50);
-  }
-
-  simulate(dt) {
-    console.log("Delta", dt);
-    if (this.x > canvas.width) {
-      this.x = -200;
-      this.vel > 6 ? (this.vel -= 1) : (this.vel = 2);
-    }
-
-    this.x += this.vel;
-  }
-
   update(dt) {
     console.log("Acc", dt);
     this.accumulator += dt;
     while (this.accumulator > this.step) {
-      this.simulate(this.step);
+      ship.update(this.step);
       this.accumulator -= this.step;
     }
   }
 }
 
 let ship = new Ship();
-ship.start();
+let game = new Game();
+game.start();
 // let lastTime = 0;
 // let accumulator = 0;
 // let step = 1 / 60;
