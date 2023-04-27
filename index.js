f79c88c7b7048333f377a83c7af0d7156237efa6
Brian Phillips <brian@briancphillips.com>
2023-04-26 19:41:51 -0500

    WIP keyboard mechanics

--- index.js
+++ index.js
@@ -1,29 +1,134 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 800;
-canvas.height = 600;
+canvas.width = 240 * 2;
+canvas.height = 320 * 2;
+
+const SHIP_SPEED = 300;
+const PRESSED = 1;
+const RELEASED = 0;
+
+class KeyboardState {
+  constructor() {
+    // Holds the current state of a given key
+    this.keyStates = new Map();
+
+    // Holds the callback functions for a key code
+    this.keyMap = new Map();
+  }
+
+  addMapping(keyCode, callback) {
+    this.keyMap.set(keyCode, callback);
+  }
+
+  handleEvent(event) {
+    const { keyCode } = event;
+
+    if (!this.keyMap.has(keyCode)) {
+      // Did not have key mapped.
+      return;
+    }
+
+    event.preventDefault();
+
+    const keyState = event.type === "keydown" ? PRESSED : RELEASED;
+
+    if (this.keyStates.get(keyCode) === keyState) {
+      return;
+    }
+
+    this.keyStates.set(keyCode, keyState);
+    //console.log(this.keyStates);
+
+    this.keyMap.get(keyCode)(keyState);
+  }
+
+  listenTo(window) {
+    ["keydown", "keyup"].forEach((eventName) => {
+      window.addEventListener(eventName, (event) => {
+        this.handleEvent(event);
+      });
+    });
+  }
+}
+
+const input = new KeyboardState();
+class SpriteSheet {
+  constructor(image, w = 32, h = 32) {
+    this.image = image;
+    this.width = w;
+    this.height = h;
+    this.tiles = new Map();
+  }
+
+  define(name, x, y) {
+    const buffer = document.createElement("canvas");
+    buffer.height = this.height;
+    buffer.width = this.width;
+    let bufferCtx = buffer.getContext("2d");
+
+    bufferCtx.drawImage(
+      this.image,
+      this.width * x,
+      this.height * y,
+      this.width,
+      this.height,
+      0,
+      0,
+      this.width,
+      this.height
+    );
+    this.tiles.set(name, buffer);
+  }
+
+  draw(name, context, x, y) {
+    const buffer = this.tiles.get(name);
+    //context.save();
+    //context.scale(2, 2);
+    context.drawImage(buffer, x, y);
+    //context.restore();
+  }
+
+  drawTile(name, context, x, y) {
+    this.draw(name, context, x * this.width, y * this.height);
+  }
+}
 
 class Ship {
   constructor() {
-    this.x = -200;
-    this.y = 100;
-    this.vel = 2;
+    this.x = canvas.width / 2 - 16;
+    this.y = canvas.height - 64;
+    this.vel = 0;
+
+    loadImage("./img/galaga2.png").then((image) => {
+      sprites = new SpriteSheet(image);
+      sprites.define("ship", 6, 0);
+      game.start();
+    });
   }
   draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "blue";
-    ctx.fillRect(this.x, this.y, 100, 50);
+    //ctx.fillStyle = "blue";
+    //ctx.fillRect(this.x, this.y, 100, 50);
+    sprites.draw("ship", ctx, this.x, this.y);
   }
 
   update(dt) {
-    console.log("Delta", dt);
-    if (this.x > canvas.width) {
-      this.x = -200;
-      this.vel > 6 ? (this.vel -= 1) : (this.vel = 2);
+    //console.log("Delta", dt);
+    if (this.x + 32 > canvas.width) {
+      this.x = canvas.width - 32;
+      this.vel = 0;
+      //this.vel > 6 ? (this.vel -= 1) : (this.vel = 1);
     }
 
-    this.x += this.vel;
+    if (this.x < 0) {
+      this.x = 0;
+      this.vel = 0;
+      //this.x = canvas.width;
+      //this.vel > 6 ? (this.vel -= 1) : (this.vel = 1);
+    }
+
+    this.x += this.vel * dt;
   }
 }
 
@@ -49,7 +154,7 @@ class Game {
   }
 
   update(dt) {
-    console.log("Acc", dt);
+    //console.log("Acc", dt);
     this.accumulator += dt;
     while (this.accumulator > this.step) {
       ship.update(this.step);
@@ -57,10 +162,46 @@ class Game {
     }
   }
 }
-
+let sprites;
 let ship = new Ship();
 let game = new Game();
-game.start();
+
+input.addMapping(37, (keyState) => {
+  if (keyState) {
+    ship.vel += -SHIP_SPEED;
+  } else {
+    ship.vel = 0;
+  }
+});
+
+input.addMapping(39, (keyState) => {
+  if (keyState) {
+    ship.vel += SHIP_SPEED;
+  } else {
+    ship.vel = 0;
+  }
+});
+
+input.listenTo(window);
+
+// window.addEventListener("keydown", (e) => {
+//   if (e.key == "ArrowLeft") {
+//     ship.vel = -SHIP_SPEED;
+//   }
+//   if (e.key == "ArrowRight") {
+//     ship.vel = SHIP_SPEED;
+//   }
+// });
+
+// window.addEventListener("keyup", (e) => {
+//   if (e.key == "ArrowLeft") {
+//     ship.vel = 0;
+//   }
+//   if (e.key == "ArrowRight") {
+//     ship.vel = 0;
+//   }
+// });
+
 // let lastTime = 0;
 // let accumulator = 0;
 // let step = 1 / 60;
